a5e9e91bc0da58d16cccd4206981a1edf6b7387c
Daniele Linaro <danielelinaro@gmail.com>
2021-08-23 17:34:22 +0200

    Added figure in Figures notebook

--- deep_utils.py
+++ deep_utils.py
@@ -1,11 +1,15 @@
 
 import os
 import re
+import sys
 import tables
 import numpy as np
+from comet_ml.api import API, APIExperiment
+from comet_ml.query import Tag
 
-
-__all__ = ['read_area_values', 'load_one_block', 'load_data_areas', 'load_data_generators', 'load_data_slide', 'predict', 'slide_window']
+__all__ = ['HashKeysDict', 'read_area_values', 'load_one_block', 'load_data_areas',
+           'load_data_generators', 'load_data_slide', 'predict', 'slide_window',
+           'collect_experiments']
 
 default_H = {
     'IEEE14': {
@@ -22,6 +26,28 @@ default_H = {
         4: 6.175
     }
 }
+
+
+class HashKeysDict (dict):
+    def __init__(self, *args):
+        dict.__init__(self, args)
+
+    def find_key(self, key):
+        if key in self:
+            return key
+        for k in self:
+            if k.startswith(key):
+                return k
+        return None
+
+    def __getitem__(self, key):
+        return dict.__getitem__(self, self.find_key(key))
+
+    def __setitem__(self, key, val):
+        k = self.find_key(key)
+        if k is None:
+            k = key
+        dict.__setitem__(self, k, val)
 
 
 def read_area_values(filename, generators_areas_map = None, generators_Pnom = None, area_measure = 'inertia'):
@@ -265,3 +291,87 @@ def predict(model, data_sliding, window_step, dt, rolling_length=50):
     H = pd.DataFrame(data).rolling(rolling_length).mean().to_numpy()
     time = np.arange(n_samples) * window_step * dt
     return time, H, y
+
+def collect_experiments(area_ID, network_name = 'IEEE39',
+                        area_measure = 'inertia',
+                        D=2, DZA=60, H_G1=500,
+                        stoch_load_bus_IDs = [3],
+                        rec_bus_IDs = [],
+                        additional_tags = [],
+                        verbose = False):
+    """
+    D - damping
+    DZA - dead-zone amplitude
+    H_G1 - inertia of generator 1
+    rec_bus_IDs - the bus(es) used for recording: an empy list means that the corresponding
+                  experiment tag won't be used
+
+    """
+    api = API(api_key = os.environ['COMET_API_KEY'])
+    workspace = 'danielelinaro'
+    project_name = 'inertia'
+
+    inertia_units = 'GW s'
+    stoch_load_bus_list = 'stoch_load_bus_' + '-'.join(map(str, stoch_load_bus_IDs))
+
+    query = Tag(network_name) & \
+            Tag('area_measure_' + area_measure) & \
+            Tag(f'D={D}') & \
+            Tag(f'DZA={DZA}') & \
+            Tag('1D_pipeline') & \
+            Tag(stoch_load_bus_list) & \
+            Tag(f'H_G1_{H_G1}') & \
+            Tag(f'area{area_ID}')
+
+    if len(rec_bus_IDs) > 1:
+        rec_bus_list = 'buses_' + '-'.join(map(str, rec_bus_IDs))
+        query &= Tag(rec_bus_list)
+
+    for tag in additional_tags:
+        if isinstance(tag, str):
+            query &= Tag(tag)
+        else:
+            query &= tag
+
+    experiments = api.query(workspace, project_name, query, archived=False)
+    n_experiments = len(experiments)
+    expts = HashKeysDict()
+    for i,experiment in enumerate(experiments):
+        ID = experiment.id
+        sys.stdout.write(f'[{i+1:02d}/{n_experiments:02d}] downloading data for experiment ID {ID}... ')
+        metrics = experiment.get_metrics()
+        sys.stdout.write('done.\n')
+        val_loss = []
+        loss = []
+        batch_loss = []
+        mape = None
+        for m in metrics:
+            if m['metricName'] == 'val_loss':
+                val_loss.append(float(m['metricValue']))
+            elif m['metricName'] == 'loss':
+                loss.append(float(m['metricValue']))
+            elif m['metricName'] == 'batch_loss':
+                batch_loss.append(float(m['metricValue']))
+            elif m['metricName'] == 'mape_prediction':
+                val = m['metricValue']
+                try:
+                    mape = float(val)
+                except:
+                    mape = np.array(list(map(float, [v for v in val[1:-1].split(' ') if len(v)])))
+        expts[ID] = {
+            'loss': np.array(loss),
+            'val_loss': np.array(val_loss),
+            'batch_loss': np.array(batch_loss),
+            'MAPE': mape,
+            'tags': experiment.get_tags()
+        }
+        if verbose:
+            print('  val_loss: {:.4f}'.format(expts[ID]['val_loss'].min()))
+            if expts[ID]['MAPE'] is not None:
+                print('      MAPE: {:.4f}%'.format(expts[ID]['MAPE']))
+            else:
+                print('      MAPE: [experiment not terminated]')
+            print('      Tags: "{}"'.format('" "'.join(expts[ID]['tags'])))
+        if expts[ID]['MAPE'] is None:
+            expts.pop(ID)
+    return expts
