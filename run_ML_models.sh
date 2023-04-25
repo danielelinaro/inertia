04258d50629b9448ed0ec9de10b0c5043ad2bbe9
Daniele Linaro <danielelinaro@gmail.com>
2023-04-25 21:07:15 +0200

    Improvements to train_ML_model.py and associated configuration files

--- run_ML_models.sh
+++ run_ML_models.sh
@@ -1,12 +1,19 @@
-#!/bin/bash
+ #!/bin/bash
 
-nreps=1
-for cfgfile in config/ML/*.json ; do
-    for i in `seq $nreps` ; do
-	logfile="${cfgfile%.json}_${i}.log"
-	python3 train_ML_model.py --no-comet $cfgfile > $logfile &
+configs=("MLP" "random_forest" "kernel_ridge" "SVR" "nearest_neighbors")
+nreps=(10 10 1 1 1)
+nfiles=${#configs[@]}
+let nfiles=nfiles-1
+for i in `seq 0 $nfiles` ; do
+    config=${configs[$i]}
+    n=${nreps[$i]}
+    for j in `seq $n` ; do
+	logfile="${config}_${j}.log"
+	python3 train_ML_model.py --no-comet config/ML/${config}.json > $logfile &
 	sleep 2
     done
-    #wait
+    if [ $n -gt 1 ] ; then
+	wait
+    fi
 done
 wait
